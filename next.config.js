41ebeed44606a5dd22f72fbf925925afb701fed9
Cynthia2019 <ccynthia2019@gmail.com>
2023-01-20 19:35:57 -0600

    load data from awsq

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
   reactStrictMode: false,
   transpilePackages: ["@mui/material"],
-  env: {
-    ACCESS_KEY: "AKIAZCLM5UPUTMIUP4P4",
-    SECRET_KEY: "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",
-    REGION: "us-east-2",
-  },
 };
 
 module.exports = nextConfig;
