76f2b37f421437f98d9f57c86965e1f5ce79150c
George Bian <bianz20@berkeley.edu>
2023-04-23 18:26:08 -0500

    Fixed umap sliders bar and text issue, only thing left to fix is process all knn of umap together

--- components/umap/paramSelector.js
+++ components/umap/paramSelector.js
@@ -1,33 +1,67 @@
-import { Slider, Row, Col } from "antd";
+import { InputNumber, Slider, Row, Col } from "antd";
 import styles from "@/styles/umap.paramSelector.module.css";
-
+import { useState } from "react";
 
 const rangeList = ["Number of neighbors"];
 
 const RangeSelector = ({ datasets, activeData, handleChange }) => {
     const data = datasets;
     const filtered = activeData;
+    const [inputValue, setInputValue] = useState(15);
+    const handleInputChange = (name, newValue) => {
+        setInputValue(newValue);
+        handleSliderChange(name, newValue);
+    };
     const handleSliderChange = (name, value) => {
+        setInputValue(value);
         handleChange(name, value);
     };
+
+    const marks = {
+        1: "1",
+        15: "15",
+        30: "30",
+    };
+
     return (
         <div className={styles["property-range"]}>
             <p className={styles["range-title"]}>Hyperparameter</p>
             {rangeList.map((name, index) => (
                 <Row key={index} justify="center" align='top' className={styles["range-row"]}>
-                    <Col pull={0} span={24}>{name}: the size of local neighborhood (in terms of number of neighboring sample points) used for manifold approximation. Larger values result in more global views of the manifold, while smaller values result in more local data being preserved. (Default: 15)</Col>
-                    <Col pull={0} span={24}>
+                    <Col
+                        pull={1}
+                        span={22}
+                        style={{fontWeight: 'bold'}}>
+                        {name}:</Col>
+
+
+                    <Col pull={1} span={22}>The size of local neighborhood (in terms of number of neighboring sample points) used for manifold approximation. Larger values result in more global views of the manifold, while smaller values result in more local data being preserved. (Default: 15)</Col>
+                    <Col pull={3} span={16}>
                         <Slider
+                            marks={marks}
                             className={styles["slider"]}
-                            range={{ draggableTrack: true }}
+                            value={inputValue}
                             defaultValue={[
                                 15
                             ]}
                             // value={[
                             //     5
                             // ]}
-                            min={5}
+                            min={1}
                             max={30}
+                            onChange={(value, filteredDatasets) =>
+                                handleSliderChange(name, value)
+                            }
+                        />
+                    </Col>
+                    <Col pull={2} span={2}>
+                        <InputNumber
+                            min={1}
+                            max={30}
+                            style={{
+                                margin: '0 16px',
+                            }}
+                            value={inputValue}
                             onChange={(value, filteredDatasets) =>
                                 handleSliderChange(name, value)
                             }
