164be891abe08bb1d1080cdab7f649791b5e04fd
Cynthia2019 <44718081+Cynthia2019@users.noreply.github.com>
2023-05-12 16:21:35 -0500

    Update aws.dev.js

--- pages/api/dist/aws.dev.js
+++ pages/api/dist/aws.dev.js
@@ -14,12 +14,6 @@ var s3Client = new _clientS.S3Client({
     secretAccessKey: process.env.NEXT_PUBLIC_SECRET_KEY
   }
 });
-var _default = s3Client; // const s3Client = new S3Client({
-//   region: 'us-east-2',
-//   credentials: {
-//     accessKeyId: 'AKIAZCLM5UPU5OGMXMKI',
-//     secretAccessKey: '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',
-//   },
-// });
+var _default = s3Client; 
 
 exports["default"] = _default;
