deea83cf954faae15805ed838b14db186e38428f
George Bian <bianz20@berkeley.edu>
2023-05-16 13:54:03 -0500

    Changed header to "Northwestern IDEAL Metamaterials Data Explorer"

--- components/shared/header.js
+++ components/shared/header.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Header() {
     return (
         <div className='header'>
-            <Link href={'/'} className='headerText'>Northwestern Engineering IDEAL LAB</Link>
+            <Link href={'/'} className='headerText'>Northwestern IDEAL Metamaterials Data Explorer</Link>
         </div>
     )
 }
